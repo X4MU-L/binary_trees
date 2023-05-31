e3ceefc823dbcfc5e0ac83c362a71c907fbcc11a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-05-31 05:02:42 +0100

    (feat) -> get the depth of a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - get the depth of a binary tree
+ * @tree: pointer to a binary tree
+ * Return: depth of @tree
+ */
+
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+	if (!tree || !tree->parent)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
