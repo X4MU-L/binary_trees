b3db39e48c2660ae98b74800ed11ab0d231a31a0
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-10 12:51:56 +0100

    (refactor) -> check only root node

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - get the height of a binary tree from a node
+ * tree_height - get the height of a binary tree from a node
  * @tree: pointer to a binary tree node
  * Return: height of @tree
  */
@@ -19,9 +19,28 @@ size_t tree_height(const binary_tree_t *tree)
 	return ((left_sum >= right_sum ? left_sum : right_sum) + 1);
 }
 
+/**
+ * min_tree_height - get the height of a binary tree from a node
+ * @tree: pointer to a binary tree node
+ * Return: height of @tree
+ */
+
+size_t min_tree_height(const binary_tree_t *tree)
+{
+	size_t left_sum, right_sum;
+
+	if (!tree)
+		return (0);
+
+	left_sum = min_tree_height(tree->left);
+	right_sum = min_tree_height(tree->right);
+
+	return ((left_sum <= right_sum ? left_sum : right_sum) + 1);
+
+
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
+ * min_tree_is_perfect - checks if a binary tree is perfect
  * @tree: the node from which to check if perfect
  * Return: 1 if perfect 0 if not or @tree is NULL
  */
@@ -33,8 +52,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_len = tree_height(tree->left);
+	left_len = min_tree_height(tree->left);
 
 	return (((tree_height(tree) - 1) == left_len) &&
-		left_len == tree_height(tree->right));
+		left_len == min_tree_height(tree->right));
 }
